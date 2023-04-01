116361ab4dc6b35ecfa8c07c462aa275c6e0e3bb
Ridwan Ikhsan <ridwan.jsx@gmail.com>
2023-04-01 10:52:00 +0700

    chore: initial release

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  if (metric.label === "web-vital") {
+    console.table(metric);
+  } else {
+    console.log(metric);
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
